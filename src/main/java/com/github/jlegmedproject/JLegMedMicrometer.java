7a70b13579e99df4bb901821995ccb48a66c9ad6
repplix <116941865+repplix@users.noreply.github.com>
2026-07-09 18:39:08 +0200

    Initial counting example with Micrometer

--- src/main/java/com/github/jlegmedproject/JLegMedMicrometer.java
+++ src/main/java/com/github/jlegmedproject/JLegMedMicrometer.java
@@ -1,12 +1,16 @@
 package com.github.jlegmedproject;
 
 import io.jexxa.jlegmed.core.JLegMed;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+
 import java.util.concurrent.TimeUnit;
+
 import static org.slf4j.LoggerFactory.getLogger;
 
 public final class JLegMedMicrometer
 {
-    public static void main(String[] args)
+    static void main()
     {
         var jLegMed = new JLegMed(JLegMedMicrometer.class);
 
@@ -15,8 +19,18 @@ public final class JLegMedMicrometer
 
                 .receive(String.class).from( () -> "Hello " )
                 .and().processWith(data -> data + "World" )
+                .and().processWith(JLegMedMicrometer::incrementCounter)
                 .and().consumeWith(data -> getLogger(JLegMedMicrometer.class).info(data));
 
         jLegMed.run();
+    }
+    static MeterRegistry registry = new SimpleMeterRegistry();
+
+    public static <T> T incrementCounter(T value)
+    {
+        var counter = registry.counter("hello.world.total");
+        counter.increment();
+        getLogger(JLegMedMicrometer.class).info("Number of greetings {}", counter.count());
+        return value;
     }
 }
