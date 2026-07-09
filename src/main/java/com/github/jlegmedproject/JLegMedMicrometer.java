55334057b65f2dff39eaf2145ebdbb030d845a5e
repplix <116941865+repplix@users.noreply.github.com>
2026-07-09 18:54:02 +0200

    Initial version of JLegMedMicrometer with Micrometer and Prometheus listening on 8080. Metrics can be queried using /metrics endpoint.

--- src/main/java/com/github/jlegmedproject/JLegMedMicrometer.java
+++ src/main/java/com/github/jlegmedproject/JLegMedMicrometer.java
@@ -1,17 +1,38 @@
 package com.github.jlegmedproject;
 
+import com.sun.net.httpserver.HttpServer;
 import io.jexxa.jlegmed.core.JLegMed;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import io.micrometer.prometheusmetrics.PrometheusConfig;
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.util.concurrent.TimeUnit;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public final class JLegMedMicrometer
 {
-    static void main()
+    static PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+
+    static void main() throws Exception
     {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/metrics", httpExchange -> {
+            // Holt die aktuellen Metriken im Prometheus-Textformat ab
+            String response = registry.scrape();
+
+            byte[] bytes = response.getBytes();
+            httpExchange.sendResponseHeaders(200, bytes.length);
+            try (OutputStream os = httpExchange.getResponseBody()) {
+                os.write(bytes);
+            }
+        });
+
+        // Server im Hintergrund starten
+        server.start();
         var jLegMed = new JLegMed(JLegMedMicrometer.class);
 
         jLegMed.newFlowGraph("HelloWorld")
@@ -24,7 +45,6 @@ public final class JLegMedMicrometer
 
         jLegMed.run();
     }
-    static MeterRegistry registry = new SimpleMeterRegistry();
 
     public static <T> T incrementCounter(T value)
     {
