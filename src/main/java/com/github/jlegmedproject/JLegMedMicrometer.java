848ab3ba60db754792cdba30c5ef6b21b1ba2c24
repplix <116941865+repplix@users.noreply.github.com>
2026-07-12 10:40:36 +0200

    Updated example to statistics provided by JLegMed and testing all 3 values

--- src/main/java/com/github/jlegmedproject/JLegMedMicrometer.java
+++ src/main/java/com/github/jlegmedproject/JLegMedMicrometer.java
@@ -4,12 +4,14 @@ import io.javalin.Javalin;
 import io.javalin.config.JavalinConfig;
 import io.javalin.micrometer.MicrometerPlugin;
 import io.jexxa.jlegmed.core.JLegMed;
+import io.jexxa.jlegmed.core.filter.FilterContext;
 import io.jexxa.jlegmed.core.flowgraph.FlowGraph;
 import io.micrometer.core.instrument.FunctionCounter;
 import io.micrometer.prometheusmetrics.PrometheusConfig;
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -31,6 +33,9 @@ public final class JLegMedMicrometer
 
                 .receive(String.class).from( () -> "Hello " )
                 .and().processWith(data -> data + "World" )
+                .and().processWith(JLegMedMicrometer::failEverySecondCall1 ).withoutProperties()
+                .and().processWith(data -> data).onError(data -> getLogger(JLegMedMicrometer.class).info("HANDLED ERROR {}", data.originalMessage()))
+                .and().processWith(JLegMedMicrometer::failEverySecondCall2 ).withoutProperties()
                 .and().consumeWith(data -> getLogger(JLegMedMicrometer.class).info(data));
 
         initMonitor(registry, jLegMed);
@@ -40,16 +45,46 @@ public final class JLegMedMicrometer
         app.stop();
     }
 
+    static AtomicInteger FAILED_COUNTER_1 = new AtomicInteger(0);
+    static AtomicInteger FAILED_COUNTER_2 = new AtomicInteger(0);
+
+    static <T> T failEverySecondCall1(T attribute, FilterContext filterContext) {
+        if ( FAILED_COUNTER_1.incrementAndGet() % 2 == 0) {
+            throw new RuntimeException("Failed Iteration");
+        }
+        return attribute;
+    }
+
+    static <T> T failEverySecondCall2(T attribute, FilterContext filterContext) {
+        if ( FAILED_COUNTER_2.incrementAndGet() % 4 == 0) {
+            throw new RuntimeException("Failed Iteration");
+        }
+        return attribute;
+    }
+
     private static void initMonitor(PrometheusMeterRegistry registry, JLegMed jLegMed) {
         var flowGraphs = jLegMed.getFlowGraphs();
         flowGraphs.forEach( flowGraphId -> initMonitor(registry, jLegMed.getFlowGraph(flowGraphId)));
     }
 
     private static void initMonitor(PrometheusMeterRegistry registry, FlowGraph flowGraph) {
-        FunctionCounter.builder("pipe.messages.total", flowGraph, value -> value.processingStats().forwardedMessages().doubleValue())
-                .description("Gesamtzahl verarbeiteter Nachrichten")
-                .tag("graph", flowGraph.flowGraphID())
-                .tag("status", "success")
+        FunctionCounter.builder("flowgraph.messages.total", flowGraph, value -> value.processingStats().forwardedMessages().doubleValue())
+                .description("Total processed messages")
+                .tag("flow_graph", flowGraph.flowGraphID())
+                .tag("result", "success")
+                .tag("error_type", "none")
+                .register(registry);
+
+        FunctionCounter.builder("flowgraph.messages.total", flowGraph, value -> value.processingStats().handledProcessingErrors().doubleValue())
+                .description("Total handled error messages")
+                .tag("result", "error")
+                .tag("error_type", "handled")
+                .register(registry);
+
+        FunctionCounter.builder("flowgraph.messages.total", flowGraph, value -> value.processingStats().unhandledProcessingErrors().doubleValue())
+                .description("Total unhandled error messages")
+                .tag("result", "error")
+                .tag("error_type", "unhandled")
                 .register(registry);
 
     }
